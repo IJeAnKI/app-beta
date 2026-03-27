93d6f148c64bf9bdd838ed1a65e4f13166cb99f1
I JeAnK I <jeankchamputis12@gmail.com>
2026-03-27 15:36:09 -0500

    Implement email uniqueness validation in user registration 2

--- src/singup.php
+++ src/singup.php
@@ -9,7 +9,7 @@ $m_phone = $_POST['mphone'];
 $p_sswd = $_POST['passwd'];
 $enc_pass = md5($p_sswd); // lo dejamos como lo tenían
 
-// ✅ VALIDACIÓN DE EMAIL (Feature 1)
+// VALIDACIÓN DE EMAIL (Feature 1)
 $check_email = "SELECT 1 FROM users WHERE email = '$e_mail'";
 $res_email = pg_query($local_conn, $check_email);
 
