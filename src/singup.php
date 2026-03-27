be5e4c2180cd4be0a8a8ebac256fb691514a51db
I JeAnK I <jeankchamputis12@gmail.com>
2026-03-27 15:28:56 -0500

    Implement email uniqueness validation in user registration

--- src/singup.php
+++ src/singup.php
@@ -1,34 +1,30 @@
 <?php
-    include('../config/database.php');
-    
-    //Get data
-    $f_name = $_POST['fname'];
-    $l_name = $_POST['lname'];
-    $e_mail = $_POST['email'];
-    $m_phone = $_POST['mphone'];
-    $p_sswd = $_POST['passwd'];
-    $enc_pass = password_hash($p_sword, PASSWORD_BCRYPT);
+include('../config/database.php');
 
-    //Query to insert into SQL
-    $sql = "INSERT INTO users (firstname, lastname, email, mobile_phone, password)
-        VALUES('$f_name', '$l_name', '$e_mail', '$m_phone', '$enc_sswd')
-    ";
+// Get data
+$f_name = $_POST['fname'];
+$l_name = $_POST['lname'];
+$e_mail = $_POST['email'];
+$m_phone = $_POST['mphone'];
+$p_sswd = $_POST['passwd'];
+$enc_pass = md5($p_sswd); // lo dejamos como lo tenían
 
-    //email
-    // VALIDAR EMAIL
-    $check_email = "SELECT 1 FROM users WHERE email = $1";
-    $res_email = pg_query_params($local_conn, $check_email, array($e_mail));
+// ✅ VALIDACIÓN DE EMAIL (Feature 1)
+$check_email = "SELECT 1 FROM users WHERE email = '$e_mail'";
+$res_email = pg_query($local_conn, $check_email);
 
-    if (pg_num_rows($res_email) > 0) {
-        echo "Error: El correo ya está registrado";
-        exit();
-    }
+if (pg_num_rows($res_email) > 0) {
+    echo "Error: El correo ya está registrado";
+    exit();
+}
 
-    
+// Query to insert into SQL
+$sql = "INSERT INTO users (firstname, lastname, email, mobile_phone, password)
+    VALUES('$f_name', '$l_name', '$e_mail', '$m_phone', '$enc_pass')
+";
 
-$res_local = pg_query($local_conn, $sql); 
+// Execute query
+pg_query($local_conn, $sql);
 
-
-    //Execute query
-    pg_query($sql);
+echo "Usuario registrado correctamente";
 ?>
