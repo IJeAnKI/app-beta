3a49a8dd5164e4611490e99da7e4ecc7fa09ceba
I JeAnK I <jeankchamputis12@gmail.com>
2026-03-27 13:13:11 -0500

    Email único

--- src/singup.php
+++ src/singup.php
@@ -15,35 +15,16 @@
     ";
 
     //email
-    $check_email = "SELECT email FROM users_model WHERE email = '$e_mail'";
-        $res_email = pg_query($local_conn, $check_email);
+    // VALIDAR EMAIL
+    $check_email = "SELECT 1 FROM users WHERE email = $1";
+    $res_email = pg_query_params($local_conn, $check_email, array($e_mail));
 
     if (pg_num_rows($res_email) > 0) {
-        echo "Error: El correo electrónico '$e_mail' ya está registrado. Por favor, use uno diferente.\n";
-    exit();
-    }
-
-    //telefono 
-    $check_phone = "SELECT mobile_phone FROM users_model WHERE mobile_phone = '$m_phone'";
-    $res_phone = pg_query($local_conn, $check_phone);
-
-    if (pg_num_rows($res_phone) > 0) {
-        echo "Error: El número de celular '$m_phone' ya está registrado en nuestro sistema."; 
+        echo "Error: El correo ya está registrado";
         exit();
     }
 
-    if ($res_local) {
-    // --- PASO B: Si funcionó el anterior, guardar en la nube (Supabase) ---
-    $res_supa = pg_query($supa_conn, $sql);
-
-    if ($res_supa) {
-        echo "¡Listo! Guardado en ambos lados.";
-    } else {
-        echo "Error: Se guardó en local pero no en la nube.";
-    }
-    } else {
-        echo "Error: No se pudo guardar ni en local.";
-    }
+    
 
 $res_local = pg_query($local_conn, $sql); 
 
